359ed7be365fb0a7272f93a1647c8252db25b40c
Aksel Õim <akseloim@gmail.com>
2023-08-28 15:34:32 +0300

    4.1

--- part4/blog/backend/models/blog.js
+++ part4/blog/backend/models/blog.js
@@ -7,10 +7,5 @@ const blogSchema = new mongoose.Schema({
     url: String,
     likes: Number
   })
-  
-  const Blog = mongoose.model('Blog', blogSchema)
-  
-  const mongoUrl = 'mongodb+srv://fullstack:Full69Stack@cluster0.ogn1kst.mongodb.net/blogApp?retryWrites=true&w=majority'
-  mongoose.connect(mongoUrl)
 
   module.exports = mongoose.model('Blog', blogSchema)
